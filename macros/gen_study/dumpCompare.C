9c4880af3cf3b4b8cb3ebf184a0b47d37758fa85
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2020-11-18 23:11:05 +0800

    dumpCompare.C: Modified and tested
    Rewrite the compare part,
    abandoning the previous complex way.
    Use dumpCompare to do the inner stuff,
    and dumpCompareMerged and dumpCompareSeperated
    to manage the files and filenames.

--- macros/gen_study/dumpCompare.C
+++ macros/gen_study/dumpCompare.C
@@ -1,8 +1,9 @@
 /// dumpCompare.C by Yueh-Shun Li (ShamrockLee)
 // Based on the dumpComparePDF.C by Prof. Shin-Shan Eiko Yu (syuvivida)
 // The function chi2NbinsCompare is borrowed directly
-// and the function dumpCompare is a reimplementation
+// and the function dumpCompareMerged is a reimplementation
 // of the function dumpComparePDF
+// that passes the arguments to dumpCompare
 
 #include <TCanvas.h>
 #include <TFile.h>
@@ -13,7 +14,12 @@
 #include <TString.h>
 #include <TStyle.h>
 #include <TMath.h>
-#include <THashList.h>
+#include <TSystem.h>
+
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <functional>
 
 void chi2NbinsCompare(const TH1* h1, const TH1* h2, double& chi2, int& nbins,
                       int binLo = -1, int binHi = -1) {
@@ -65,8 +71,8 @@ void chi2NbinsCompare(const TH1* h1, const TH1* h2, double& chi2, int& nbins,
 }
 
 void drawHistsCompared(TH1* h1, TH1* h2, const TString endfix1, const TString endfix2, Bool_t normalize = true) {
-  Int_t nBins1 = h1->GetNbinsX();
-  Int_t nBins2 = h2->GetNbinsX();
+  Int_t nEntries1 = h1->GetEntries();
+  Int_t nEntries2 = h2->GetEntries();
   TLegend* leg = new TLegend(0.3333, 0.7027, 0.8333, 0.9023);
   leg->SetFillColor(0);
   leg->SetFillStyle(0);
@@ -87,13 +93,13 @@ void drawHistsCompared(TH1* h1, TH1* h2, const TString endfix1, const TString en
   h1->Sumw2();
   h2->Sumw2();
   if (normalize) {
-    if (nBins1) h1->Scale(1.0 / h1->Integral());
-    if (nBins2) h2->Scale(1.0 / h2->Integral());
+    if (nEntries1) h1->Scale(1.0 / h1->Integral());
+    if (nEntries2) h2->Scale(1.0 / h2->Integral());
   }
 
   Double_t chi2 = 0.;
   Int_t nbins = 0;
-  if (nBins1 && nBins2) {
+  if (nEntries1 && nEntries2) {
     Float_t max =
     h1->GetMaximum() > h2->GetMaximum() ? h1->GetMaximum() : h2->GetMaximum();
     h1->SetMaximum(1.1 * max);
@@ -107,7 +113,7 @@ void drawHistsCompared(TH1* h1, TH1* h2, const TString endfix1, const TString en
 
   leg->Clear();
   leg->SetHeader("");
-  if (nBins1 && nBins2) {
+  if (nEntries1 && nEntries2) {
     leg->AddEntry((TObject*)0, Form("#chi^{2}/NDF=%.1f/%d", chi2, nbins), "");
     leg->AddEntry((TObject*)0,
                   Form("#chi^{2} Prob=%.2f", TMath::Prob(chi2, nbins)), "");
@@ -151,97 +157,159 @@ void refgetIdxHistOpenableRecent(TDirectory *tdir, std::vector<TString>& vName, 
   }
 }
 
-template<UInt_t nTDir>
-std::vector<UInt_t[nTDir]> getVArrIdxCommon(std::vector<TString> arrVName[nTDir], std::vector<UInt_t> arrVIdx[nTDir]) {
-  std::vector<UInt_t[nTDir]> vArrIdxCommon;
-  vArrIdxCommon.clear();
-  UInt_t arrIdxIdx[nTDir];
-  UInt_t arrSizeIdx[nTDir];
+void dumpCompare(TDirectory *tdir1, TDirectory *tdir2, Bool_t normalize, TString endfix1, TString endfix2, std::function<void (TCanvas*, TString, UInt_t)> funPrint, std::function<void (TCanvas*, UInt_t)> funEndPrint, Int_t indexOrder=1) {
+  const UInt_t nTDir = 2;
+  TDirectory *arrTDirIn[nTDir] = {tdir1, tdir2};
+  std::vector<TString> vNameMain;
+  std::vector<UInt_t> vIdxMain;
+  UInt_t indexChooseLeaves = (indexOrder == 0) ? 0 : indexOrder - 1;
+  refgetIdxHistOpenableRecent(arrTDirIn[indexChooseLeaves], vNameMain, vIdxMain);
+  std::vector<UInt_t> vpermIdx(vIdxMain.size());
+  std::iota(vpermIdx.begin(), vpermIdx.end(), 0);
+  if (indexOrder == 0) {
+    std::sort(vpermIdx.begin(), vpermIdx.end(), [&vIdxMain](UInt_t i, UInt_t j)->Bool_t {return vIdxMain[i] < vIdxMain[j];});
+  }
+  TCanvas *c1 = new TCanvas;
+  UInt_t nPage = 0;
+  for (UInt_t perm:vpermIdx) {
+    TString nameHist = vNameMain[perm];
+    TH1 *arrHist[nTDir];
+    Bool_t isLeafCommon = true;
+    for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
+      TObject *histRaw = arrTDirIn[iTDir]->Get(nameHist);
+      if (histRaw == nullptr || !histRaw->IsA()->InheritsFrom("TH1")) {
+        isLeafCommon = false;
+        std::cout << "Leaf " << nameHist << " not found in " << arrTDirIn[iTDir]->GetName() << "(" << arrTDirIn[iTDir] << ")" << std::endl;
+      } else {
+        arrHist[iTDir] = (TH1 *) histRaw;
+      }
+    }
+    if (!isLeafCommon) continue;
+    drawHistsCompared(arrHist[0], arrHist[1], endfix1, endfix2, normalize);
+    funPrint(c1, nameHist, nPage);
+    nPage++;
+  }
+  funEndPrint(c1, nPage);
+}
+
+void dumpCompareMerged(TDirectory *tdir1, TDirectory *tdir2, Bool_t normalize, TString endfix1, TString endfix2, TString pathDir="fig", TString nameFile="", TString optionPrint="pdf", Int_t indexOrder=1) {
+  // const UInt_t nTDir = 2;
+  // TDirectory *arrTDirIn[nTDir] = {tdir1, tdir2};
+  // std::vector<TString> vNameMain;
+  // std::vector<UInt_t> vIdxMain;
+  // UInt_t indexChooseLeaves = (indexOrder == 0) ? 0 : indexOrder - 1;
+  // refgetIdxHistOpenableRecent(arrTDirIn[indexChooseLeaves], vNameMain, vIdxMain);
+  // std::vector<UInt_t> vpermIdx(vIdxMain.size());
+  // std::iota(vpermIdx.begin(), vpermIdx.end(), 0);
+  // if (indexOrder == 0) {
+  //   std::sort(vpermIdx.begin(), vpermIdx.end(), [&vIdxMain](UInt_t i, UInt_t j)->Bool_t {return vIdxMain[i] < vIdxMain[j];});
+  // }
+  if (nameFile == "") {
+    nameFile = endfix1 + "_" + endfix2 + "." + optionPrint;
+    if (nameFile.EndsWith("+")) {
+      nameFile.Resize(nameFile.Length()-1);
+    }
+  }
+  
+  if (pathDir.Length() >= 2 && pathDir.EndsWith("/")) {
+    pathDir.Resize(pathDir.Length()-1);
+  }
+  TString filenamePrint = (pathDir == "" ? "" : pathDir + "/") + nameFile;
+  TCanvas *c1 = new TCanvas;
+  std::function<void (TCanvas *, TString, UInt_t)> funPrint = [filenamePrint](TCanvas *c, TString nameHist, UInt_t iPage) {
+    if (iPage == 0) {
+      c->Print(filenamePrint + "(");
+    } else {
+      c->Print(filenamePrint);
+    }
+  };
+  std::function<void (TCanvas *, UInt_t)> funEndPrint = [filenamePrint](TCanvas *c, UInt_t nPage) {
+    c->Print(filenamePrint + ")");
+  };
+  dumpCompare(tdir1, tdir2, normalize, endfix1, endfix2, funPrint, funEndPrint, indexOrder);
+  // UInt_t nPage = 0;
+  // for (UInt_t perm:vpermIdx) {
+  //   TString nameLeaf = vNameMain[perm];
+  //   TH1 *arrHist[nTDir];
+  //   Bool_t isLeafCommon = true;
+  //   for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
+  //     TObject *histRaw = arrTDirIn[iTDir]->Get(nameLeaf);
+  //     if (histRaw == nullptr || !histRaw->IsA()->InheritsFrom("TH1")) {
+  //       isLeafCommon = false;
+  //       std::cout << "Leaf " << nameLeaf << " not found in " << arrTDirIn[iTDir]->GetName() << "(" << arrTDirIn[iTDir] << ")" << std::endl;
+  //     } else {
+  //       arrHist[iTDir] = (TH1 *) histRaw;
+  //     }
+  //   }
+  //   if (!isLeafCommon) continue;
+  //   drawHistsCompared(arrHist[0], arrHist[1], endfix1, endfix2, normalize);
+  //   c1->Print(filenamePrint + ((nPage == 0) ? "(" : ""), optionPrint);
+  // }
+  // c1->Print(filenamePrint + ")", optionPrint);
+}
+
+void dumpCompareMerged(TString pathFileIn1, TString pathFileIn2, Bool_t normalize=true, TString endfix1="", TString endfix2="", TString nameDir="fig", TString nameFile="", TString optionPrint="pdf", Int_t indexOrder=1) {
+  const UInt_t nTDir = 2;
+  TString arrPathFileIn[nTDir] = {pathFileIn1, pathFileIn2};
+  TString arrEndfix[nTDir] = {endfix1, endfix2};
+  TFile *arrTFileIn[nTDir];
   for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
-    arrIdxIdx[iTDir] = 0;
-    arrSizeIdx[iTDir] = arrVIdx[iTDir].size();
-    if (arrSizeIdx[iTDir] == 0) {
-      return vArrIdxCommon;
+    if (arrEndfix[iTDir] == "") {
+      arrEndfix[iTDir]=gSystem->GetFromPipe(Form("file=%s; test=${file##*/}; echo \"${test%%.root}\"", arrPathFileIn[iTDir].Data()));
+      arrTFileIn[iTDir] = TFile::Open(arrPathFileIn[iTDir], "read");
     }
-  };
-  auto getName = [&arrVName, &arrVIdx](UInt_t iTDir, UInt_t ii)->TString {
-    return arrVName[iTDir][arrVIdx[iTDir][ii]];
-  };
-  Bool_t finish = false;
-  while (true) {
-    TString nameMax = getName(0, arrIdxIdx[0]);
-    for (UInt_t iTDir=1; iTDir<nTDir; iTDir++) {
-      if (nameMax < getName(iTDir, arrIdxIdx[iTDir])) {
-        nameMax = getName(iTDir, arrIdxIdx[iTDir]);
-      }
-    }
-    Bool_t findCommon = false;
-    while (!findCommon) {
-      UInt_t iTDir = 0;
-      for (iTDir=0; iTDir<nTDir; iTDir++) {
-        while (getName(iTDir, arrIdxIdx[iTDir]) < nameMax) {
-          arrIdxIdx[iTDir]++;
-          if (arrIdxIdx[iTDir] >= arrSizeIdx[iTDir]) {
-            return vArrIdxCommon; // finish
-          }
-        }
-        if (getName(iTDir, arrIdxIdx[iTDir]) > nameMax) {
-          nameMax = getName(iTDir, arrIdxIdx[iTDir]);
-          break;
-        }
-      }
-      findCommon = (iTDir == nTDir);
-    }
-    UInt_t arrIdxCommon[nTDir];
-    for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
-      arrIdxCommon[iTDir] = arrVIdx[iTDir][arrIdxIdx[iTDir]];
-    }
-    vArrIdxCommon.push_back(arrIdxCommon);
-    for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
-      arrIdxIdx[iTDir]++;
-      if (arrIdxIdx[iTDir] >= arrSizeIdx[iTDir]) {
-        return vArrIdxCommon; // finish
-      }
-    }
-    // for (UInt_t iTdir=0; iTdir<nTDir; iTdir++) {
-    //   if (arrIdxIdx[iTdir] >= arrSizeIdx[iTdir]) {
-    //     finish = true;
-    //     break;
-    //   }
-    // }
-    // if (finish) break;
+  }
+  dumpCompareMerged(arrTFileIn[0], arrTFileIn[1], normalize, arrEndfix[0], arrEndfix[1], nameDir, nameFile, optionPrint, indexOrder);
+  for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
+    arrTFileIn[iTDir]->Close();
   }
 }
 
-void dumpCompareMerged(TDirectory *tdir1, TDirectory *tdir2, TString endfix1, TString endfix2, TString nameDir="fig", TString nameFile="", TString optionPrint="pdf", Bool_t normalize=true, Int_t indexOrder=1) {
-  const UInt_t nTDir = 2;
-  TDirectory *arrTDirIn[nTDir] = {tdir1, tdir2};
-  std::vector<TString> arrVName[nTDir];
-  std::vector<UInt_t> arrVIdx[nTDir];
-  THashList *arrTListKeys[nTDir];
-  for (UInt_t i=0; i<nTDir; i++) {
-    refgetIdxHistOpenableRecent(arrTDirIn[i], arrVName[i], arrVIdx[i]);
-    arrTListKeys[i] = (THashList *) arrTDirIn[i]->GetListOfKeys();
+void dumpCompareSeperated(TDirectory *tdir1, TDirectory *tdir2, Bool_t normalize, TString endfix1, TString endfix2, TString pathDir="", std::function<TString (TString, UInt_t)> funNameFile=nullptr, TString optionPrint="svg", Int_t indexOrder=1) {
+  if (pathDir == "") {
+    pathDir = endfix1 + "_" + endfix2;
   }
-  std::vector<UInt_t[nTDir]> vArrIdxCommon = getVArrIdxCommon<nTDir>(arrVName, arrVIdx);
-  if (indexOrder != 0) {
-    std::sort(vArrIdxCommon.begin(), vArrIdxCommon.end(), [&arrVName, &arrVIdx, &indexOrder](UInt_t arrIdxCommon[nTDir])->UInt_t {
-      return arrIdxCommon[indexOrder-1];
-    });
-  }
-  if (nameFile = "") {
-    nameFile = endfix1 + "_" + endfix2;
-  }
-  TString filenamePrint = (nameDir == "" ? "" : nameDir + "/") + nameFile;
-  TCanvas *c1 = new TCanvas;
-  UInt_t nPage = 0;
-  for (auto arrIdxCommon: vArrIdxCommon) {
-    TH1 *arrHist[nTDir];
-    for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
-      arrHist[iTDir] = (TH1 *) arrTListKeys[iTDir]->At(arrIdxCommon[iTDir]);
+  if (funNameFile == nullptr) {
+    TString suffixFromOption = optionPrint;
+    if (suffixFromOption.EndsWith("+")) {
+      suffixFromOption.Resize(suffixFromOption.Length()-1);
     }
-    drawHistsCompared(arrHist[0], arrHist[1], endfix1, endfix2, normalize);
-    c1->Print(nPage ? filenamePrint : (filenamePrint + "("), optionPrint);
+    funNameFile = [suffixFromOption](TString nameHist, UInt_t iPage)->TString{return nameHist + "." + suffixFromOption;};
   }
-  c1->Print(filenamePrint + ")", optionPrint);
+  if (pathDir.Length() >= 2 && pathDir.EndsWith("/")) {
+    pathDir.Resize(pathDir.Length()-1);
+  }
+  gSystem->mkdir(pathDir);
+  std::function<void (TCanvas *, TString, UInt_t)> funPrint = [pathDir, endfix1, endfix2, funNameFile, optionPrint](TCanvas *c, TString nameHist, UInt_t iPage) {
+    c->Print((pathDir=="" ? "" : pathDir + "/") + funNameFile(nameHist, iPage), optionPrint);
+  };
+  std::function<void (TCanvas *, UInt_t)> funEndPrint = [](TCanvas *c, UInt_t nPage){};
+  dumpCompare(tdir1, tdir2, normalize, endfix1, endfix1, funPrint, funEndPrint, indexOrder);
+}
+
+void dumpCompareSeperated(TString pathFileIn1, TString pathFileIn2, Bool_t normalize=true, TString endfix1="", TString endfix2="", TString pathDir="", std::function<TString (TString, UInt_t)> funNameFile=nullptr, TString optionPrint="svg", Int_t indexOrder=1) {
+  const UInt_t nTDir = 2;
+  TString arrPathFileIn[nTDir] = {pathFileIn1, pathFileIn2};
+  TString arrEndfix[nTDir] = {endfix1, endfix2};
+  TFile *arrTFileIn[nTDir];
+  for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
+    if (arrEndfix[iTDir] == "") {
+      arrEndfix[iTDir]=gSystem->GetFromPipe(Form("file=%s; test=${file##*/}; echo \"${test%%.root}\"", arrPathFileIn[iTDir].Data()));
+      arrTFileIn[iTDir] = TFile::Open(arrPathFileIn[iTDir], "read");
+    }
+  }
+  dumpCompareSeperated(arrTFileIn[0], arrTFileIn[1], normalize, arrEndfix[0], arrEndfix[1], pathDir, funNameFile, optionPrint, indexOrder);
+  for (UInt_t iTDir=0; iTDir<nTDir; iTDir++) {
+    arrTFileIn[iTDir]->Close();
+  }
+}
+
+void dumpCompareSeperated(TDirectory *tdir1, TDirectory *tdir2, Bool_t normalize, TString endfix1, TString endfix2, std::function<TString (TString, TString)> funPathDir, std::function<TString (TString, UInt_t)> funNameFile=nullptr, TString optionPrint="svg", Int_t indexOrder=1) {
+  TString pathDir = funPathDir(endfix1, endfix2);
+  dumpCompareSeperated(tdir1, tdir2, normalize, endfix1, endfix2, pathDir, funNameFile, optionPrint, indexOrder);
+}
+
+void dumpCompareSeperated(TString nameFileIn1, TString nameFileIn2, Bool_t normalize, TString endfix1, TString endfix2, std::function<TString (TString, TString)> funPathDir, std::function<TString (TString, UInt_t)> funNameFile=nullptr, TString optionPrint="svg", Int_t indexOrder=1) {
+  TString pathDir = funPathDir(endfix1, endfix2);
+  dumpCompareSeperated(nameFileIn1, nameFileIn2, normalize, endfix1, endfix2, pathDir, funNameFile, optionPrint, indexOrder);
 }
